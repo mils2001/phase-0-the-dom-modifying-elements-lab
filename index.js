2e1102f00dcd1367e6a79651f0267fe6debacc3e
chris <Mileschris466@gmail.com>
2024-10-05 23:57:20 +0300

    java tags

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+console.log(document.getElementById("main"));
+main.remove(); 
+
+const newHeader = document.createElement("h1");
+newHeader.innerHTML = "h1";
+newHeader.id = "victory";
+newHeader.textContent= "Miles is the champion";
